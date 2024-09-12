80603dc71ad1b49aa6ef615107d97a89b3f88281
kjone8371 <kjone8371@gmail.com>
2024-09-12 09:27:21 +0900

    백준 1051문제

---
+++ src/BackJun/Back_Jun1051.java
@@ -0,0 +1,13 @@
+package BackJun;
+
+import java.util.Scanner;
+
+public class Back_Jun1051 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+
+    }
+}
